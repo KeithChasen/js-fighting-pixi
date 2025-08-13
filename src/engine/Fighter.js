9330d25beefa6bea20b4b940fc50744d8d65073b
keithchasen <keithchasen89@gmail.com>
2025-08-13 21:48:36 +0200

    simple fighter

--- src/engine/Fighter.js
+++ src/engine/Fighter.js
@@ -1,9 +1,29 @@
-export class Fighter {
+import { Container, Sprite, Texture } from 'pixi.js';
+export class Fighter extends Container {
 	id = null;
-	position = null;
+	// position = { x: 0, y: 0 };
 	health = 100;
+	// sprite = null;
 
-	update() {}
+	constructor() {
+		super();
+		// this.position = position;
+		// this.sprite = new Sprite(Texture.WHITE);
+		// this.sprite.width = 100;
+		// this.sprite.height = 200;
+		// this.sprite.position.set(100, 200);
+
+		const sprite = new Sprite(Texture.WHITE);
+		sprite.width = 100;
+		sprite.height = 200;
+		sprite.position.set(100, 200);
+
+		this.addChild(sprite);
+	}
+
+	update() {
+		// console.log(this.sprite);
+	}
 
 	onHit(damage) {
 		this.health -= damage;
--- src/engine/Game.js
+++ src/engine/Game.js
@@ -1,8 +1,11 @@
 import { Application } from 'pixi.js';
 import { InputManager } from './InputManager';
+import { Fighter } from './Fighter';
 
 export class Game {
 	app = null;
+
+	fighter = null;
 
 	inputManager = new InputManager();
 
@@ -15,14 +18,18 @@ export class Game {
 
 		this.app.canvas.style.position = 'absolute';
 
+		this.fighter = new Fighter();
+
+		this.app.stage.addChild(this.fighter);
+
 		document.body.appendChild(this.app.canvas);
 	}
 
 	start() {
-		console.log(this.app, 'this app');
 		this.app &&
 			this.app.ticker.add(() => {
 				this.inputManager.update();
+				this.fighter.update();
 			});
 	}
 }
